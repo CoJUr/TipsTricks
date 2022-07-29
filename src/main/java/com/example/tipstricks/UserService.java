060f67e1496d6e55e2dbdcc0734af451bb3294a3
“Cory <coryurton@gmail.com>
2022-07-29 16:54:43 -0500

    Initial commit

---
+++ src/main/java/com/example/tipstricks/UserService.java
@@ -0,0 +1,2 @@
+package com.example.tipstricks;public class UserService {
+}
---
+++ src/test/java/com/example/tipstricks/TipsTricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tipstricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsTricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
